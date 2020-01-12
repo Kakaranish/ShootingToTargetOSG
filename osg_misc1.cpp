b471bf12ef8f89a4d315a594e06f97caf727c7fa
kakaranish <stasiekgruz@gmail.com>
2020-01-12 23:50:32 +0100

    Some changes

--- osg_misc1.cpp
+++ osg_misc1.cpp
@@ -18,6 +18,9 @@ osg::Vec4f getColor(int r, int g, int b, int alpha = 255)
 osg::ref_ptr<osg::MatrixTransform> createShootingTarget(osg::Vec3f position, float radius, float thickness = 0.05f)
 {
     const float xAxisRotationAngle = osg::PI_2;
+    float stickLength = radius / 5. * 4.;
+    position += osg::Vec3f(0, 0, 1 * (radius + stickLength));
+
     osg::ref_ptr<osg::Cylinder> blueCylinderShape = new osg::Cylinder(
         position + osg::Vec3f(0, 0, 0), radius, thickness);
     blueCylinderShape->setRotation(osg::Quat(xAxisRotationAngle, osg::X_AXIS));
@@ -25,21 +28,27 @@ osg::ref_ptr<osg::MatrixTransform> createShootingTarget(osg::Vec3f position, flo
     blueCylinder->setColor(getColor(30, 144, 255, 255));
 
     osg::ref_ptr<osg::Cylinder> redCylinderShape = new osg::Cylinder(
-        position + osg::Vec3f(0, - thickness, 0), radius / 3. * 2, 0.05f);
+        position + osg::Vec3f(0, -thickness, 0), radius / 3. * 2, 0.05f);
     redCylinderShape->setRotation(osg::Quat(xAxisRotationAngle, osg::X_AXIS));
     osg::ref_ptr<osg::ShapeDrawable> redCylinder = new osg::ShapeDrawable(redCylinderShape);
-    redCylinder->setColor(getColor(255, 0, 0, 255));
+    redCylinder->setColor(getColor(255, 0, 0));
 
     osg::ref_ptr<osg::Cylinder> yellowCylinderShape = new osg::Cylinder(
         position + osg::Vec3f(0, -(thickness + 0.05f), 0), radius / 3., 0.05f);
     yellowCylinderShape->setRotation(osg::Quat(xAxisRotationAngle, osg::X_AXIS));
     osg::ref_ptr<osg::ShapeDrawable> yellowCylinder = new osg::ShapeDrawable(yellowCylinderShape);
-    yellowCylinder->setColor(getColor(255, 255, 0, 255));
+    yellowCylinder->setColor(getColor(255, 255, 0));
+
+    osg::ref_ptr<osg::ShapeDrawable> stickDrawable = new osg::ShapeDrawable(
+        new osg::Box(osg::Vec3(position.x(), position.y(), position.z() - 1 * (radius + stickLength / 2.f)),
+                     thickness * 3, thickness, stickLength));
+    stickDrawable->setColor(getColor(139,69,19));
 
     osg::ref_ptr<osg::MatrixTransform> matrixTransform = new osg::MatrixTransform;
     matrixTransform->addChild(blueCylinder);
     matrixTransform->addChild(redCylinder);
     matrixTransform->addChild(yellowCylinder);
+    matrixTransform->addChild(stickDrawable);
     return matrixTransform.release();
 }
 
@@ -49,17 +58,17 @@ osg::ref_ptr<osg::Geode> createCoordinateSystem(float distanceFromZero, float sp
 
     osg::ref_ptr<osg::ShapeDrawable> xPoint = new osg::ShapeDrawable(
         new osg::Sphere(osg::Vec3f(distanceFromZero, 0, 0), sphereRadius));
-    xPoint->setColor(getColor(255, 0, 0, 255));
+    xPoint->setColor(getColor(255, 0, 0));
     geode->addDrawable(xPoint);
 
     osg::ref_ptr<osg::ShapeDrawable> yPoint = new osg::ShapeDrawable(
         new osg::Sphere(osg::Vec3f(0, distanceFromZero, 0), sphereRadius));
-    yPoint->setColor(getColor(0, 255, 0, 255));
+    yPoint->setColor(getColor(0, 255, 0));
     geode->addDrawable(yPoint);
 
     osg::ref_ptr<osg::ShapeDrawable> zPoint = new osg::ShapeDrawable(
         new osg::Sphere(osg::Vec3f(0, 0, distanceFromZero), sphereRadius));
-    zPoint->setColor(getColor(0, 0, 255, 255));
+    zPoint->setColor(getColor(0, 0, 255));
     geode->addDrawable(zPoint);
 
     return geode.release();
@@ -67,10 +76,12 @@ osg::ref_ptr<osg::Geode> createCoordinateSystem(float distanceFromZero, float sp
 
 osg::ref_ptr<osg::Geode> createGround(float width = 15.f)
 {
+    const float axisRotationAngle = osg::PI;
     osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-    osg::ref_ptr<osg::ShapeDrawable> groundDrawable = new osg::ShapeDrawable(
-        new osg::Box(osg::Vec3(0.0f, 0.0f, 0.0f), width, width, 0.05f));
-    groundDrawable->setColor(getColor(127,255,0));
+    osg::ref_ptr<osg::Box> box = new osg::Box(osg::Vec3(0.0f, 0.0f, 0.0f), width, width, 0.05f);
+    box->setRotation(osg::Quat(axisRotationAngle, osg::X_AXIS));
+    osg::ref_ptr<osg::ShapeDrawable> groundDrawable(new osg::ShapeDrawable(box));
+    groundDrawable->setColor(getColor(127, 255, 0));
     geode->addDrawable(groundDrawable);
     return geode;
 }
@@ -79,12 +90,13 @@ int main(int argc, char const *argv[])
 {
     osg::ref_ptr<osg::Group> root = new osg::Group;
     osg::ref_ptr<osg::Geode> coordinateSystem = createCoordinateSystem(10, 0.5f);
-    osg::ref_ptr<osg::Geode> ground = createGround();
-    osg::ref_ptr<osg::MatrixTransform> shootingTarget = createShootingTarget(osg::Vec3f(0, 0, 0), 3, 0.1f);
+    osg::ref_ptr<osg::Geode> ground = createGround(15);
+    osg::ref_ptr<osg::MatrixTransform> shootingTarget = createShootingTarget(
+        osg::Vec3f(0, 2.f, 0), 1.5, 0.1f);
 
+    root->addChild(ground);
     root->addChild(shootingTarget);
     root->addChild(coordinateSystem);
-    root->addChild(ground);
 
     osgViewer::Viewer viewer;
     viewer.setUpViewInWindow(100, 100, 800, 600);
